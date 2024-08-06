80a838da313beb42e8638a5b8f99314ed0c3eb1a
Suriyo05 <suriyo_lumsom@cmu.ac.th>
2024-08-06 22:14:05 +0700

    lab10 finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import axios from "axios";
+import { cleanUser } from "@/libs/cleanUser";
 import { useState } from "react";
+import { UserCardProps } from "@/libs/types";
+import UserCard from "@/components/UserCard";
 
 export default function RandomUserPage() {
   // annotate type for users state variable
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState<UserCardProps[]>([]);
 
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
@@ -19,6 +22,9 @@ export default function RandomUserPage() {
     const users = resp.data.results;
 
     //Your code here
+    
+    setUsers(users.map((user:any) => cleanUser(user)));
+
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
   };
@@ -32,7 +38,7 @@ export default function RandomUserPage() {
           className="form-control text-center"
           style={{ maxWidth: "100px" }}
           type="number"
-          onChange={(e) => setGenAmount(e.target.value)}
+          onChange={(e) => setGenAmount(Number(e.target.value))}//เปลี่ยนstringเป็นnumber
           value={genAmount}
         />
         <button className="btn btn-dark" onClick={generateBtnOnClick}>
@@ -42,7 +48,7 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users && !isLoading && users.map((user: UserCardProps) => <UserCard name={user.name} imgUrl={user.imgUrl} address={user.address} email={user.email} key={user.email} />)}
     </div>
   );
 }
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,3 +1,13 @@
 // add type for UserCard's Props
-
+interface UserCardProps{
+    name: string;
+    imgUrl: string;
+    address: string;
+    email: string;
+}
 // add type for UserCardDetail's Props
+interface UserCardDetail{
+    email: string;
+    address: string;
+}
+export type{UserCardProps,UserCardDetail}
